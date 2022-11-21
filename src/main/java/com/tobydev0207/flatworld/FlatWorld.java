f2fb56f2ba774ae18e54403a1e6ba114554799c8
TobyDev0207 <118341149+TobyDev0207@users.noreply.github.com>
2022-11-21 15:34:06 +0700

    Format codes

--- src/main/java/com/tobydev0207/flatworld/FlatWorld.java
+++ src/main/java/com/tobydev0207/flatworld/FlatWorld.java
@@ -10,26 +10,28 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 
 public class FlatWorld implements ModInitializer {
-    public static final String MOD_ID = "flatworld";
+  public static final String MOD_ID = "flatworld";
 
-    private static final RegistryKey<World> FLAT_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY, new Identifier(FlatWorld.MOD_ID, "flat_dimension"));
+  private static final RegistryKey<World> FLAT_DIMENSION_KEY =
+      RegistryKey.of(Registry.WORLD_KEY, new Identifier(FlatWorld.MOD_ID, "flat_dimension"));
 
-    @Override
-    public void onInitialize() {
-        registerDimension();
-        registerPortal();
-    }
+  @Override
+  public void onInitialize() {
+    registerDimension();
+    registerPortal();
+  }
 
-    private void registerDimension() {
-        RegistryKey.of(Registry.DIMENSION_TYPE_KEY, FLAT_DIMENSION_KEY.getValue());
-    }
-    private void registerPortal() {
-        CustomPortalBuilder.beginPortal()
-                .frameBlock(Blocks.STONE)
-                .lightWithItem(Items.IRON_INGOT)
-                .destDimID(FLAT_DIMENSION_KEY.getValue())
-                .tintColor(149, 165, 166)
-                .onlyLightInOverworld()
-                .registerPortal();
-    }
+  private void registerDimension() {
+    RegistryKey.of(Registry.DIMENSION_TYPE_KEY, FLAT_DIMENSION_KEY.getValue());
+  }
+
+  private void registerPortal() {
+    CustomPortalBuilder.beginPortal()
+        .frameBlock(Blocks.STONE)
+        .lightWithItem(Items.IRON_INGOT)
+        .destDimID(FLAT_DIMENSION_KEY.getValue())
+        .tintColor(149, 165, 166)
+        .onlyLightInOverworld()
+        .registerPortal();
+  }
 }
