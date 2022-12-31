aefe5dc96bf0e7b0d925c978886f6ea8f75ac6c2
TobyDev0207 <118341149+TobyDev0207@users.noreply.github.com>
2022-12-31 13:09:56 +0700

    Add guide book

--- src/main/java/org/goldpiggymc/flatworld/ItemManager.java
+++ src/main/java/org/goldpiggymc/flatworld/ItemManager.java
@@ -1,10 +1,9 @@
 package org.goldpiggymc.flatworld;
 
+import io.wispforest.owo.itemgroup.OwoItemSettings;
 import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 
 public class ItemManager implements ItemRegistryContainer {
-  public static final Item HEART_OF_THE_EARTH =
-      new Item(new FabricItemSettings().group(Vars.FLAT_WORLD_GROUP));
+  public static final Item HEART_OF_THE_EARTH = new Item(new OwoItemSettings().group(Vars.FLAT_WORLD_GROUP));
 }
